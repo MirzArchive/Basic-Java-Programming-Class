73856a91095a9584b8c1ce1f76455dacacabccfc
MIRZA HILMI <exquisitemirza@student.ub.ac.id>
2022-11-21 13:12:10 +0700

    created new variable from DiceRoller object

---
+++ OOP_ObjectUtilization.java
@@ -0,0 +1,6 @@
+public class OOP_ObjectUtilization {
+    public static void main(String[] args) {
+        OOP_DiceRoller diceRoll = new OOP_DiceRoller();
+        System.out.printf("Random number : %d",diceRoll.roll(20, 69));
+    }
+}
