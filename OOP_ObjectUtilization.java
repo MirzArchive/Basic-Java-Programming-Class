6151258d6fe706d7bfad669f58deff0be3587312
MIRZA HILMI <exquisitemirza@student.ub.ac.id>
2022-11-21 19:00:44 +0700

    --name-status

--- OOP_ObjectUtilization.java
+++ OOP_ObjectUtilization.java
@@ -5,5 +5,8 @@ public class OOP_ObjectUtilization {
         OOP_Static friend3 = new OOP_Static("Squidward");
 
         System.out.printf("%d",OOP_Static.numberOfFriends);
+        System.out.printf("Name : %s",friend1.name);
+        System.out.printf("Name : %s",friend2.name);
+        System.out.printf("Name : %s",friend3.name);
     }
 }
