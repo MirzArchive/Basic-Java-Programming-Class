ce75f5c8384a41459c79590e2aa9c85ad01033ad
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2022-11-24 20:26:07 +0700

    print superhuman attributes in class method

--- OOP/ObjectUtilize.java
+++ OOP/ObjectUtilize.java
@@ -2,5 +2,6 @@ package OOP;
 public class ObjectUtilize {
     public static void main(String[] args) {
         SuperHuman human1 = new SuperHuman("Batman", 81, "$$$");
+        System.out.printf("%s",human1.getData());
     }
 }
