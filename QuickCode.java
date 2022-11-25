f6f2ee813517f2247bd9ad8dd0427a6ca2667a02
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2022-11-25 11:41:45 +0700

    check even odd with binary

--- QuickCode.java
+++ QuickCode.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class QuickCode {
     public static void main(String[] args) {
-        
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.printf("%s",((n & 1) == 0 ? "EVEN" : "ODD"));
     }
 }
